b46f44325472a28b7a3ccceb92a80562e4eefa86
Zevsoverlord <mr_a953@mail.ru>
2020-05-22 09:58:22 +0300

    add Application creator

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,14 +3,15 @@
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-nav>
         <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/equipments" v-if="isLoggedIn">Equipments</b-nav-item>
+        <b-nav-item to="/equipment" v-if="isLoggedIn">Equipments</b-nav-item>
+        <b-nav-item to="/application" v-if="isLoggedIn">Applications</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
       <auth v-if="!isLoggedIn"/>
        <div class="container" v-else>
       <div class="row">
       <div class="col-sm-12">
-        <span class="mr-auto">{{getUsername}}</span>
+        <span class="mr-auto">{{ getUsername }}</span>
         <button type="button" class="btn btn-dark btn-sm"  @click="logout">Logout</button>
         </div>
       </div>
@@ -35,7 +36,7 @@ export default {
       return this.$store.getters.isLoggedIn;
     },
     getUsername() {
-      return this.$store.getters.getUsername();
+      return this.$store.getters.getUsername;
     },
   },
   methods: {
---
+++ frontend/src/components/Application/ApplicationsList.vue
@@ -0,0 +1,156 @@
+<template>
+ <div class="container">
+    <h1>Applications</h1>
+    <b-button class="btn btn-success" v-b-modal.application-modal>
+      <b-icon icon="plus" aria-hidden="true" ></b-icon> New Application
+    </b-button>
+  <div class="tab-content custom-scrollbar">
+      <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Title</th>
+              <th scope="col">Description</th>
+              <th scope="col">Status</th>
+              <th scope="col">Executor</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="inf in app" :key="inf.id">
+              <td>{{ inf.id }}</td>
+              <td>{{  inf.title }}</td>
+              <td>{{ inf.description }}</td>
+              <td>{{inf.status}}</td>
+              <td>{{inf.executor}}</td>
+              <td>
+                <b-button variant="danger" @click="onDeleteEvent(inf)">
+                  <b-icon icon="trash" aria-hidden="true">
+                  </b-icon>Delete
+                </b-button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+   <b-modal ref="ApplicationModal"
+         id="application-modal"
+         title="New Application"
+         hide-footer
+         hide-backdrop
+         centered>
+        <b-form @submit="Submit" @reset="Reset" class="w-100">
+        <b-form-group id="form-title-group"
+                    label="Title:"
+                    label-for="form-title-input">
+          <b-form-input id="form-title-input"
+                        type="text"
+                        v-model="Application.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-description-group"
+                      label="Description:"
+                      label-for="form-description-input">
+            <b-form-textarea id="form-description-input"
+                          v-model="Application.description"
+                          required
+                          placeholder="Enter description">
+            </b-form-textarea>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+      </b-modal>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ApplicationsList',
+  data() {
+    return {
+      Application: {
+        title: '',
+        description: '',
+      },
+      app: [{
+        id: 1,
+        title: 'Проблема с подключением',
+        description: 'Не могу подключиться к базе данных компании',
+        status: 'В процессе',
+        executor: 'Иван Иванов',
+      }],
+    };
+  },
+  methods: {
+    getApplications() {
+      const path = '';
+      axios.get(path, {
+        headers: { Authorization: `Bearer ${this.$store.state.token}` },
+      }).then((resp) => {
+        this.applications = resp.data;
+      }).catch(err => console.log(err));
+    },
+    InitForm() {
+      this.Application.title = '';
+      this.Application.description = '';
+    },
+    Reset(evt) {
+      evt.preventDefault();
+      this.$refs.ApplicationModal.hide();
+      this.initForm();
+    },
+    Submit(evt) {
+      evt.preventDefault();
+      this.$refs.ApplicationModal.hide();
+      const payload = {
+        title: this.Application.title,
+        description: this.Application.description,
+      }
+      this.sendApplication(payload);
+      this.initForm();
+    },
+    sendApplication(payload) {
+      const path = '';
+      axios.post(path, payload, {
+        headers: { Authorization: `Bearer ${this.$store.state.token}` },
+      })
+        .then(() => {
+          this.getApplications();
+        }).catch((err) => {
+          console.log(err);
+          this.getApplications();
+        });
+    },
+    removeApplication(appID) {
+      const path = `http://localhost:1984/delete/${appID}`;
+      axios.delete(path, {
+        headers: { Authorization: `Bearer ${this.$store.state.token}` },
+      })
+        .then(() => {
+          this.getApplications();
+        })
+        .catch((error) => {
+        // eslint-отключение следующей строки
+          console.error(error);
+          this.getApplications();
+        });
+    },
+    onDeleteEvent(application) {
+      this.removeApplication(application.id);
+    },
+  },
+  created() {
+    this.InitForm();
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
